151cbb05323d5583cd172d2dbb362918e5911836
Gustavo Morales <gustavo.morales@gmail.com>
2017-07-12 10:20:40 -0500

    Add photo field for Post with Paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711234845) do
+ActiveRecord::Schema.define(version: 20170712015013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20170711234845) do
     t.string "description", limit: 128
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
