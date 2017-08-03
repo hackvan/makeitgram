35cae317ae0ba83c9d9f96a0ffcabaf33e8685bf
Diego Camacho <hackvan@gmail.com>
2017-08-02 19:52:24 -0500

    Adding a comment functionality on views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,17 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-    @comment.save
+    if @comment.save
+      respond_to :js
+    else
+      redirect_to root_path, flash: {error: "The comment can't be blank"}
+    end
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:text)
+      params.require(:comment)
+        .permit(:text)
         .merge(user_id: current_user.id, post_id: params[:id])
     end
 end
